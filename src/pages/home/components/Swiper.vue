2af860a06e7a41738faa407bd113f55ac8750ecb
NULL <ltobenull@gmail.com>
2019-01-05 15:48:44 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,20 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide>I'm Slide 1</swiper-slide>
+    <swiper-slide>I'm Slide 2</swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
