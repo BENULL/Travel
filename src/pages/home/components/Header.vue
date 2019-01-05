bca14e11e35a708b07e6b1043189e1bf887dbd6f
NULL <ltobenull@gmail.com>
2019-01-05 14:26:06 +0800

    improve add iconfont

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,26 +21,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
- .header
-  display: flex
-  line-height: .86rem
-  color: #ffffff
-  background: #00bcd4
-  .header-left
-    float: left
-    width: .64rem
-  .header-input
-    flex: 1
-    height: .64rem
-    line-height: .64rem
-    margin-left: .2rem
-    margin-top: .12rem
-    background: #ffffff
-    border-radius: .1rem
-    color: #cccccc
-  .header-right
-    width: 1.24rem
-    float: right
-    text-align: center
-
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    color: #ffffff
+    background: $bgColor
+    .header-left
+      float: left
+      width: .64rem
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      padding-left: .2rem
+      margin-left: .2rem
+      margin-top: .12rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #cccccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
